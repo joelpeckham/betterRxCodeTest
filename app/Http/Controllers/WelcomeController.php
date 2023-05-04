842e314df00e30d59bd9a8e00cc2ca075090ca1a
Joel Peckham <mail@jpeckham.com>
2023-05-04 22:37:12 +0000

    extra validation for very dumb users

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -67,6 +67,13 @@ class WelcomeController extends Controller
             throw $error;
         }
 
+        if ($response->failed()) {
+            $error = \Illuminate\Validation\ValidationException::withMessages([
+                "firstName" => "API Call Failed",
+            ]);
+            throw $error;
+        }
+
         return Inertia::render("Welcome", [
             "searchData" => [
                 "req" => $request,
