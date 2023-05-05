86f92bd31d1e61ad2ff9b4a7d9e35acfce4194be
Joel Peckham <mail@jpeckham.com>
2023-05-05 02:04:59 +0000

    Slower Fade in return to top elem

--- resources/js/Components/ReturnToTop.jsx
+++ resources/js/Components/ReturnToTop.jsx
@@ -12,12 +12,15 @@ const ReturnToTop = (sectionId) => {
     }, []);
     return (
         <div
-            className={`fixed bottom-0 right-0 mb-4 mr-4 transition-opacity ${
+            className={`fixed bottom-0 right-0 mb-4 mr-4 transition-opacity duration-500 ${
                 scrollPosition > 450 ? "opacity-100" : "opacity-0"
             }`}
         >
             <a href={`#${sectionId}`} aria-label="Return to Top">
-                <button className="rounded-full bg-indigo-600 px-4 py-2 font-bold text-white hover:bg-indigo-500" aria-label="Return to Top">
+                <button
+                    className="rounded-full bg-indigo-600 px-4 py-2 font-bold text-white hover:bg-indigo-500"
+                    aria-label="Return to Top"
+                >
                     <svg
                         className="h-6 w-6"
                         fill="none"
